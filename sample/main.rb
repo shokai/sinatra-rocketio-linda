efaa2dc5b16b266797ed71dcd59d6f0550c9d77b
Sho Hashimoto <hashimoto@shokai.org>
2013-05-20 03:26:22 +0900

    print all tuple operations in sample

--- sample/main.rb
+++ sample/main.rb
@@ -9,6 +9,10 @@ io.on :disconnect do |client|
   puts "bye <#{client}>"
 end
 
+io.on :* do |event, data, client|
+  puts "#{event} - #{data}  <#{client}>" if event.to_s =~ /linda/
+end
+
 linda.on :error do |err|
   STDERR.puts err
 end
