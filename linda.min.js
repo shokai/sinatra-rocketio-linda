f67a8d971c6f17da52e47d9c2b224fa594c618f3
Sho Hashimoto <hashimoto@shokai.org>
2013-10-10 19:08:32 +0900

    release v1.0.1

--- linda.min.js
+++ linda.min.js
@@ -1,4 +1,4 @@
-// Linda.js v1.0.0 (rocketio v0.3.0)
+// Linda.js v1.0.1 (rocketio v0.3.3)
 // https://github.com/shokai/sinatra-rocketio-linda
 // (c) 2013 Sho Hashimoto <hashimoto@shokai.org>
 // The MIT License
